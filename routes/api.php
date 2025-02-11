1acf32c58a7abe68b587be562c0fd593445386e5
Laden8210 <domingo.laden@gmail.com>
2025-02-12 03:12:09 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SMSController;
+use App\Http\Controllers\SensorDataController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,6 @@ Route::post('/send-sms', [SMSController::class, 'sendSMS']);
 
 Route::get('/get-sms', [SMSController::class, 'getSMS']);
 Route::put('/update-status/{id}', [SMSController::class, 'updateStatus']);
+
+Route::post('/sensor-data', [SensorDataController::class, 'store']);
+Route::get('/sensor-data', [SensorDataController::class, 'index']);
